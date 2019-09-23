c837cac789b54fe9b17899f843858e9294319c10
watosar/nekojyarasi <38435692+watosar@users.noreply.github.com>
2019-09-23 22:29:49 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 apk add build-base libffi-dev libsodium-dev ffmpeg
 python3 -m pip install -r requirements.txt
-apk del build-base libffi-dev libsodium-dev
+# apk del build-base libffi-dev libsodium-dev
