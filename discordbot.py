a47406ae599f4c7018831789b5376dbd217261c8
watosar/nekojyarasi <38435692+watosar@users.noreply.github.com>
2019-09-23 17:39:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
 import os
+import logging
 import traceback
 
+logging.basicConfig(level=logging.INFO)
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
