c1a2c9db8aca8852a52c2ac0e80293586d540838
watosar/nekojyarasi <38435692+watosar@users.noreply.github.com>
2019-09-23 21:52:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,12 +2,19 @@ from discord.ext import commands
 import os
 import logging
 import traceback
+import io
+import sys
+from contextlib import redirect_stdout
+import textwrap
 
 
 logging.basicConfig(level=logging.INFO)
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.event
+async def on_ready():
+    print(f'logged on as {bot.user}')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,6 +24,63 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+def cleanup_code(content):
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    # remove `foo`
+    return content.strip('` \n')
+
+_ = None
+@bot.command()
+async def eval(ctx, *, body: str):
+    """Evaluates a code"""
+    global _
+    env = {
+        'bot': bot,
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+        '_': _
+    }
+    
+    env.update(globals())
+
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        try:
+            await ctx.message.add_reaction('\u2705')
+        except:
+            pass
+
+        if ret is None:
+            if value:
+                await ctx.send(f'```py\n{value}\n```')
+        else:
+            _ = ret
+            await ctx.send(f'```py\n{value}{ret}\n```')
 
 
 @bot.command()
